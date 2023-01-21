a07eb84d588145acf4c69d28a606d411e5636586
pavspurs <atlantis_pro@naver.com>
2023-01-21 12:01:34 +0900

    1.1v

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import AppRouter from 'components/Router';
+import { authService } from "fBase";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn}/>
+      <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
+    </>
+  );
+}
+
+export default App;
